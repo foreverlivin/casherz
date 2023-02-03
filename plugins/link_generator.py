8e275f7cea3cef6d3a933512d97dbd7f1fe19a55
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-03 23:53:44 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -6,10 +6,7 @@ from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
 
-ownerx_id = 1443454117
-adminx_id = 1863307059
-idk_id = 1972662787
-@Bot.on_message(filters.private & filters.user(ownerx_id, adminx_id, idk_id) & filters.command('batch'))
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
@@ -43,7 +40,7 @@ async def batch(client: Client, message: Message):
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
-@Bot.on_message(filters.private & filters.user(ownerx_id, adminx_id, idk_id) & filters.command('genlink'))
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:

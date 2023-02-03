f761bd1e45e05aff288bee83542a8735175ec442
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-03 21:52:16 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -3,10 +3,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
-from config import ADMINS, OWNER_ID
+from config import ADMINS
 from helper_func import encode, get_message_id
 
-@Bot.on_message(filters.private & filters.user(OWNER_ID) & filters.command('batch'))
+OWNERX_ID = 1443454117
+
+@Bot.on_message(filters.private & filters.user(OWNERX_ID) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
@@ -40,7 +42,7 @@ async def batch(client: Client, message: Message):
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
-@Bot.on_message(filters.private & filters.user(OWNER_ID) & filters.command('genlink'))
+@Bot.on_message(filters.private & filters.user(OWNERX_ID) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
